f2b72e19c4ff0f242602244486322d66ded61753
danielaparker <danielaparker@yahoo.com>
2016-02-24 19:42:02 -0500

    Added jcr_tests.cpp

---
+++ test_suite/src/jcr_tests.cpp
@@ -0,0 +1,56 @@
+// Copyright 2013 Daniel Parker
+// Distributed under Boost license
+
+#ifdef __linux__
+#define BOOST_TEST_DYN_LINK
+#endif
+
+#include <boost/test/unit_test.hpp>
+#include <sstream>
+#include <vector>
+#include <utility>
+#include <ctime>
+#include <new>
+#include "jsoncons/json.hpp"
+#include "jsoncons_ext/jcr/jcr.hpp"
+
+using namespace jsoncons;
+using namespace jsoncons::jcr;
+
+BOOST_AUTO_TEST_SUITE(jcr_test_suite)
+
+struct jcr_fixture
+{
+};
+
+BOOST_AUTO_TEST_CASE(test_jcr)
+{
+    json_schema schema = json_schema::parse(R"(
+    {
+        "line-count" : 3426,
+        "word-count" : 27886
+    }
+    )");
+
+    json val1 = json::parse(R"(
+    {
+        "line-count" : 3426,
+        "word-count" : 27886
+    }
+    )");
+    json val2 = json::parse(R"(
+    {
+        "line-count" : 3426,
+        "word-count" : 27887
+    }
+    )");
+
+    BOOST_CHECK(schema.validate(val1));
+    BOOST_CHECK(!schema.validate(val2));
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
+
+
